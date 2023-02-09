cd62f65c10b7245496a4e5ebce6e730fd772c196
Ahmed Kechicheb <ahmedkechicheb13@gmail.com>
2023-02-09 20:58:14 +0100

    get all workouts in frontend

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,25 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
-
+const cors = require("cors");
+const bodyParser = require("body-parser");
 // express app
 const app = express();
+
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use("/api/workouts", workoutRoutes);
+
 
 // connect to db
 mongoose.set("strictQuery", false);
@@ -16,18 +32,7 @@ mongoose
   .then((result) => {
     // listen to port
     app.listen(process.env.PORT, () =>
-    console.log(`connected to db AND listening on port`, process.env.PORT)
+      console.log(`connected to db AND listening on port`, process.env.PORT)
     );
   })
   .catch((err) => console.log(err));
-  
-  // middleware
-  app.use(express.json());
-
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
-
-// routes
-app.use("/api/workouts", workoutRoutes);
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,31 @@
-const Home = () => {
+import { useEffect, useState } from "react"
 
+const Home = () => {
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(() => {
+      const fetchWorkouts = async () => {
+        const response = await fetch('/api/workouts')
+        const json = await response.json()
+  
+        if (response.ok) {
+          setWorkouts(json)
+        }
+      }
+  
+      fetchWorkouts()
+    }, [])
     return (
       <div className="home">
-        <h2>Home</h2>
+            <div className="workouts">
+        {workouts && workouts.map(workout => (
+          <div  key={workout._id} >
+            
+            
+            {workout.title}
+             </div>
+        ))}
+      </div>
       </div>
     )
   }
