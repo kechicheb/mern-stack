214d31578a2a670e2478ff57b3f2e68cee393700
Ahmed Kechicheb <ahmedkechicheb13@gmail.com>
2023-02-09 13:01:35 +0100

    connected to db MongoDB Atlas & Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,35 @@
-require('dotenv').config()
-
-const express = require('express')
-const workoutRoutes = require('./routes/workouts')
+require("dotenv").config();
+const express = require("express");
+const workoutRoutes = require("./routes/workouts");
+const mongoose = require("mongoose");
 
 // express app
-const app = express()
+const app = express();
+
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    app.listen(process.env.PORT, () =>
+      console.log(`connected to db AND listening on port`,process.env.PORT)
+    );
+  })
+
+  .catch((err) => console.log(err));
 
 // middleware
-app.use(express.json())
-
+app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 // routes
-app.use('/api/workouts', workoutRoutes)
+app.use("/api/workouts", workoutRoutes);
 
 // listen for requests
-app.listen(process.env.PORT, () => console.log(`listeneing on for port 4000`));
+// app.listen(process.env.PORT, () => console.log(`listeneing on for port 4000`));
